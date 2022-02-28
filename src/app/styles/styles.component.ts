4a487e5db89e82306b63fbe735f79baa6a871684
kerrybli <kerry.blig12@gmail.com>
2022-02-28 12:10:34 +0530

    added ngStyles condional bg color

--- src/app/styles/styles.component.ts
+++ src/app/styles/styles.component.ts
@@ -1,8 +1,20 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-styles',
-    templateUrl: './styles.component.html'
+  selector: 'app-styles',
+  templateUrl: './styles.component.html',
 })
+export class StylesComponent {
+  serverStatus: boolean = false;
+  serverOnline: string = 'offline';
 
-export class StylesComponent{}
+  constructor() {
+    setTimeout(() => {
+      this.serverStatus = true;
+    }, 200);
+    this.serverOnline = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  getColor() {
+    return this.serverOnline === 'online' ? 'green' : 'red';
+  }
+}
