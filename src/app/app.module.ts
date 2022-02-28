02987e8befd96de774b42c5d9bcaf0af9bf0f969
kerrybli <kerry.blig12@gmail.com>
2022-02-28 11:52:46 +0530

    added styles component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StylesComponent } from './styles/styles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StylesComponent
   ],
   imports: [
     BrowserModule
